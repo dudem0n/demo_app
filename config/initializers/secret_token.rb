5f82b2bedce0a6f8b249c01d72a4c52601470be0
Andrew Macintosh <andrewjmacintosh@gmail.com>
2014-03-12 13:27:09 -0700

    Finish static pages

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoApp::Application.config.secret_key_base = '2c506ea422b89a70cf206125779f29c326b6dc57600134ddc31316ccc1077961bd8131ec3c3ec09ef0bf25a210d71af2f49e8779e159bbb8e47f986a719a96a4'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+DemoApp::Application.config.secret_key_base = secure_token
