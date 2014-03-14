878d39db6da0bfefdf4333d5f1acd8ab6d52679a
Andrew Macintosh <andrewjmacintosh@gmail.com>
2014-03-14 10:17:58 -0700

    chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
   @users = User.paginate(page: params[:page])
@@ -37,7 +38,20 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+  
+
+
+  #ONLY PRIVATE SHIT HERE
+
   private
+
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -57,4 +71,9 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
+
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe User do
 
-  it {should respond_to(:password_confirmation)}
-  it {should respond_to(:remember_token)}
-  it {should respond_to(:authenticate)}
+
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
@@ -12,14 +10,18 @@ describe User do
 
   subject { @user }
 
-  it { should respond_to(:authenticate)}
 
   it { should respond_to(:name)}
   it { should respond_to(:email)}
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
-  it { should be_valid }
+  it {should respond_to(:password_confirmation)}
+  it {should respond_to(:remember_token)}
+  it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
+  it {should be_valid }
+  it {should_not be_admin}
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -105,4 +107,15 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 end
