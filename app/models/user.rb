5ff08277a312b9c7ebea1a951ddc044657e624eb
Andrew Macintosh <andrewjmacintosh@gmail.com>
2014-03-14 14:16:35 -0700

    added user following - were done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
+
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   validates :password, length: { minimum: 6}
 
@@ -23,9 +28,30 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+
+
+  #DONT PUT SHIT HERE YOU WHORE
+
   private
 
   def create_remember_token
     self.remember_token = User.hash(User.new_remember_token)
   end
+
 end
